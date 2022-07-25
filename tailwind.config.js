d569dcb595f31633f5453423448a53b8e644ba1e
jeong-mi <wjdal6951@naver.com>
2022-07-25 18:42:30 +0900

    feat: 초기 개발환경 세팅
    - styled-component, tailwindss 설치

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        checked: "#A0F592",
+        plus: "#8AE2A8",
+        cancel: "#F18086",
+      },
+    },
+  },
+  plugins: [],
+};
